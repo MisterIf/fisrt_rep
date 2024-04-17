1ec581ad63546219f681496c08db3066d444a1b4
pavlo <pkrupitsa@gmail.com>
2024-04-17 20:25:00 +0200

    var1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def add_numbers():
     num1 = int(input('Enter number 1 - '))
     num2 = int(input('Enter number 2 - '))
-    result = add_numbers(num1, num2)
+    result = num1 + num2
     return result
 
 
